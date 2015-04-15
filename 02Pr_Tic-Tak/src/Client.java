9698726cf359e26abf6cc832340b22b8684f2e9d
DianaSusharnyk <DinkaSushik@gmail.com>
2015-04-15 10:59:34 +0300

    delete some code

--- 02Pr_Tic-Tak/src/Client.java
+++ 02Pr_Tic-Tak/src/Client.java
@@ -8,22 +8,7 @@ public class Client {
 		InetAddress address = InetAddress.getByName(null);
 		System.out.println("Address = "+ address);
 		Socket socket = new Socket(address, Server.PORT);
-		try{
-			System.out.println("Socket = " + socket);
-			InputStream byteIn = socket.getInputStream();
-			BufferedReader in = new BufferedReader(new InputStreamReader(byteIn));
-			PrintWriter out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())),true);
-			
-			out.println("pass");
-			String serverAnswer = in.readLine();
-			if(serverAnswer.equals("correct")){
-				FileOutputStream fout = new FileOutputStream(new File(".//1//"+in.readLine()));
-				int b = byteIn.read();
-				while(b != -1){
-					fout.write(b);
-					b = byteIn.read();
-				}
-				fout.close();
+		
 			}else{
 				System.out.println("Password is incorrect.");
 			}
